34c1a8d394c47b71b838bf8f97aef0af3a5a637f
TomasMacula <tomasmacula@live.com>
2026-05-09 10:28:23 +0200

    funkcia upload, test cez postman: - post - http://127.0.0.1:5000/upload - body/form-data - key->'file' - value-> vybrat lokalny txt subor - test cez metodu get(funkcia list_files)

--- app.py
+++ app.py
@@ -14,5 +14,11 @@ def list_files():
     files = os.listdir(STORAGE_DIR)
     return jsonify(files), 200
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    file.save(os.path.join(STORAGE_DIR, file.filename))
+    return jsonify({"message": "upload succesfull", "filename": file.filename}), 201
+
 if __name__ == '__main__':
     app.run()
