b8c945cf16572c1d945684d2211f95b0b231a564
TomasMacula <tomasmacula@live.com>
2026-05-09 10:11:37 +0200

    vytvorenie priecinku 'cloud_storage'

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import os
 
 app = Flask(__name__)
+STORAGE_DIR = 'cloud_storage'
+os.makedirs(STORAGE_DIR, exist_ok=True)
 
 @app.route('/')
 def home():
@@ -9,7 +11,7 @@ def home():
 
 @app.route('/files')
 def list_files():
-    files = os.listdir()
+    files = os.listdir(STORAGE_DIR)
     return jsonify(files), 200
 
 if __name__ == '__main__':
